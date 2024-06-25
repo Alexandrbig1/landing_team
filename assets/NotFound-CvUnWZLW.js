e1963056580d538597a2d9a5717c7a0c9fad02a2
Alexandrbig1 <alexsmagin1@gmail.com>
2024-06-24 19:33:32 -0500

    Updates

--- assets/NotFound-CvUnWZLW.js
+++ assets/NotFound-CvUnWZLW.js
@@ -1,4 +1,4 @@
-import{p as t,a as o,j as e,H as r,b as i}from"./index-CWmqVVDO.js";const s=t.div`
+import{p as t,a as o,j as e,H as r,b as i}from"./index-B1JyL36k.js";const s=t.div`
   display: flex;
   flex-direction: column;
   align-items: center;
