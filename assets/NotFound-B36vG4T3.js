45ddbbc091bd803b6a53caa0a215ef1b5d6630d4
Alexandrbig1 <alexsmagin1@gmail.com>
2024-06-27 12:09:42 -0500

    Updates

--- assets/NotFound-B36vG4T3.js
+++ assets/NotFound-B36vG4T3.js
@@ -1,4 +1,4 @@
-import{p as t,a as o,j as e,H as r,b as i}from"./index-BMLmTkKR.js";const s=t.div`
+import{p as t,a as o,j as e,H as r,b as i}from"./index-C6KLTG5W.js";const s=t.div`
   display: flex;
   flex-direction: column;
   align-items: center;
