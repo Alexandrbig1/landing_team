6693a93a9453d41b329029eca8386b7bd6a1ad06
Alexandrbig1 <alexsmagin1@gmail.com>
2024-07-08 12:46:53 -0500

    feat: added scroll button and functionality, to scroll up

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,20 +5,20 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import { Main } from "../components/layout";
 import MenuBurger from "../components/MenuBurger/MenuBurger";
+import ScrollBtn from "../components/UI/ScrollBtn/ScrollBtn";
 
 // eslint-disable-next-line react/prop-types
 function RootLayout() {
-  // function RootLayout({ toggleTheme, isDarkTheme }) {
   return (
     <div>
       <Header />
       <MenuBurger />
-      {/* <Header toggleTheme={toggleTheme} isDarkTheme={isDarkTheme} /> */}
       <Main>
         <Suspense fallback={<Loader />}>
           <Outlet />
         </Suspense>
       </Main>
+      <ScrollBtn />
       <Footer />
     </div>
   );
